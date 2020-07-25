2c6088fd83b01fa5701a7f3d12eb031d2c3a66f8
Dawid Dziurla <dawidd0811@gmail.com>
2020-07-25 12:12:17 +0200

    import

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
-# Git try push Github Action
+# Git try push GitHub Action
 
-An action that tries to make a `git push`. If it does not succeed, then wait some time and try again. The number of maximum tries is controllable via `tries` input.
+An action that tries to make a `git push`. If it does not succeed, then wait some time, perform `git pull --rebase` and try again.
+The number of maximum tries is controllable via `tries` input.
 
 ## Usage
 
 ```yaml
 - name: Try pushing
-  uses: dawidd6/action-git-try-push@master
+  uses: dawidd6/action-git-try-push@v1
   with:
     # Optional, used for pushing
     token: ${{github.token}}
     # Optional, defaults to current directory
     directory: path/to/repo
-    # Optional, defaults to 10
+    # Optional, defaults to:
+    remote: origin
+    # Optional, defaults to:
+    branch: master
+    # Optional, defaults to:
     tries: 20
 ```
--- main.js
+++ main.js
@@ -4,27 +4,42 @@ const exec = require('@actions/exec')
 async function main() {
     try {
         const token = core.getInput("token")
-        const tries = core.getInput("tries") || 10
         const directory = core.getInput("directory")
+        const remote = core.getInput("remote", { required: true })
+        const branch = core.getInput("branch", { required: true })
+        const tries = core.getInput("tries", { required: true })
 
+        // Change directory.
         if (directory) {
             process.chdir(directory)
         }
 
+        // Set up token authentication for pushing.
+        // Command taken from actions/checkout.
         if (token) {
-            await exec.exec("git", ["config", "--local", "http.https://github.com/.extraheader", `AUTHORIZATION: basic ${token}`])
+            await exec.exec("git", ["config", "--local", "http.https://github.com/.extraheader", `AUTHORIZATION: basic x-access-token:${token}`])
         }
 
+        // Checkout the branch which should be pushed.
+        await exec.exec("git", ["checkout", branch])
+
+        // Loop specified number of tries.
         for (let i = 0; i < tries; i++) {
             try {
-                await exec.exec("git", ["push"])
+                // Try to push, if successful, then checkout previous branch and just exit.
+                await exec.exec("git", ["push", remote, branch])
+                await exec.exec("git", ["checkout", "-"])
                 return
             } catch (error) {
-                await exec.exec("git", ["pull", "--rebase"])
+                // Push failed. Wait some time, pull changes with rebasing and try again.
+                const delay = Math.floor(Math.random() * (10 + i)) + 3
+                await exec.exec("sleep", [delay])
+                await exec.exec("git", ["pull", "--rebase", remote, branch])
             }
         }
 
-        throw "Max tries reached"
+        // This should never be reached.
+        throw new Error("Max tries reached")
     } catch (error) {
         core.setFailed(error.message)
     }
